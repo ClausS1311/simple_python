8ffc39d09632c81cb6fc20dad45afe49d8e5e69a
ClausS1311 <klaudiu98@yahoo.com>
2025-09-01 18:39:31 +0300

    Update functie.py

--- functie.py
+++ functie.py
@@ -1,7 +1,6 @@
 def functie(num1, num2):
     return (num1+num2)/2
 
-
 nr1 = int(input("nr este "))
 nr2 = int(input("nr este "))
 print(functie(nr1, nr2))
@@ -15,4 +14,5 @@ def functie2(n1,n2):
         return (a1+a2)*(a1*a2)
     return func3(n1,n2)
 print(functie2(nr1,nr2))
+
 
